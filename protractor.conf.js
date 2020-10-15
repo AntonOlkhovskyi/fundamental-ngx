a7dbcd18065eb9b77d815d829df2bccd7fe4ae78
Sean Moore <sean.moore@valor-software.com>
2020-10-15 18:37:07 +0300

    test(e2e): add tests for Link component

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,32 +6,19 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*el.e2e-spec.ts'
+    './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'https://sap.github.io/fundamental-ngx/#',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-
-    plugins: [
-    /*    {
-            displayHelpUrl: true, // Displays the aXe help URL along with the error. Defaults to true.
-            displayContext: true, // Displays the HTML of interest. Defaults to true.
-            displayPasses: true, // Display pass results. Defaults to true.
-            displayViolations: true, // Display vioaltions. Defaults to true.
-            standardsToReport: ['wcag2a', 'wcag2aa'], // A list of standards to report on. If empty, reports on all standards.
-            ignoreAxeFailures: false, // If true, aXe failures won't cause the whole test to fail. Defaults to false
-            package: 'protractor-axe-html-report-plugin',
-            globalParams: {}
-
-  }*/],
   onPrepare() {
       browser.driver.manage().window().maximize();
       require('ts-node').register({
